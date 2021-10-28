aaf3a170a929ac0862115254e9a0ba588e0783e6
JulianSanti28 <juliansmartinez@unicauca.edu.co>
2021-10-28 11:49:59 -0500

    martinez_peña: Práctica 1 Polimorfismo

---
+++ hyun/primeraPractica/Martinez_Peña/primeraPractica/src/main/java/Model/Viaje.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 57322
+ */
+public abstract class Viaje {
+
+    protected String origen;
+    protected String destino;
+    protected int costo;
+    protected Date fechaSalida;
+    protected Date fechaLlegada;
+
+    /**
+     * Constructor parametrizado
+     *
+     * @param origen
+     * @param destino
+     * @param costo
+     * @param fechaSalida
+     * @param fechaLlegada
+     */
+    public Viaje(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        this.origen = origen;
+        this.destino = destino;
+        this.costo = costo;
+        this.fechaSalida = fechaSalida;
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    /**
+     * Constructor vacío
+     */
+    public Viaje() {
+
+    }
+
+    //Getters and Setters
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Date getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(Date fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    /**
+     * Método abstracto
+     *
+     * @return
+     */
+    public abstract String descripcion();
+
+    /**
+     *
+     * @return
+     */
+    public String cualquierMetodo() {
+        return "Cualquier método implementado en la clase base";
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String cualquierMetodo2() {
+        return "Cualquier método2 implementado en la clase base";
+    }
+}
