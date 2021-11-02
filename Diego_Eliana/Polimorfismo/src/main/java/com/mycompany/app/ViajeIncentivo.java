d33ea1acecf54d8c5fbc124052e24d780782c6aa
UnSup_ <collazosdie@unicauca.edu.co>
2021-11-02 10:47:04 -0500

    Diego_Collazos-Eliana_Cerquera-Polimorfismo

---
+++ Diego_Eliana/Polimorfismo/src/main/java/com/mycompany/app/ViajeIncentivo.java
@@ -0,0 +1,35 @@
+package com.mycompany.app;
+
+import java.util.Date;
+
+public class ViajeIncentivo extends Viaje {
+	/**
+	* Empresa que patrocina el viaje al empleado
+	*/
+	private String empresa;
+	
+	//Constructor, getters and setters
+	public ViajeIncentivo() {
+		super();
+	}
+	public ViajeIncentivo(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, String empresa) {
+		super(origen, destino, costo, fechaSalida, fechaLlegada);
+		this.empresa = empresa;
+	}
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
+	@Override
+	public String descripcion() {
+		return "Viaje incentivo que te envia la empresa " + empresa;
+	}
+
+	public String cualquierMetodo2() {
+		return "M�todo implementado en la clase hija viaje de incentivo";
+	}
+}
