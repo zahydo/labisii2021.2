a60e850479be33332feccc336cbdcc3f504a22e6
acrip <jazapata@unicauca.edu.co>
2021-11-03 09:23:54 -0500

    AndresZapata-Polimorfismo: first commit on branch Andres_Zapata_Polimorfismo. Se agrega la primera practica y se actualiza el archivo readme

---
+++ AndresZapata/PrimeraPractica/src/main/java/co/unicauca/tallerpolimorfismo/modelo/ViajeFamiliar.java
@@ -0,0 +1,25 @@
+package co.unicauca.tallerpolimorfismo.modelo;
+import java.util.Date;
+
+public class ViajeFamiliar extends Viaje {
+    private int familia;
+
+    public ViajeFamiliar(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, int familia){
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+        this.familia = familia;
+    }   
+    public int getFamilia() {
+        return familia;
+    }
+    public void setFamilia(int familia) {
+        this.familia = familia;
+    }
+    @Override
+    public String descripcion(){
+        return "Viaje para disfrutar con toda tu familia";
+    }
+    @Override
+    public String cualquierMetodo2(){
+        return "Método implementado en la clase hija viaje familiar";
+    }
+}
