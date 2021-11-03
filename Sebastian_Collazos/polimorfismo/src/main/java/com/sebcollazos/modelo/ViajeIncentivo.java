caee171b8152d11941a44d9b7c14bcb8a44ee6f8
secoveGordo <sebcollazos@unicauca.edu.co>
2021-11-02 23:09:53 -0500

    primera practica

---
+++ Sebastian_Collazos/polimorfismo/src/main/java/com/sebcollazos/modelo/ViajeIncentivo.java
@@ -0,0 +1,36 @@
+package com.sebcollazos.modelo;
+
+import java.util.Date;
+
+public class ViajeIncentivo extends Viaje {
+
+    /**
+     * Empresa que patrocina el viaje al empleado
+     */
+    private String empresa;
+
+    public ViajeIncentivo(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada,
+            String empresa) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+        this.empresa = empresa;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    @Override
+    public String descripcion() {
+        return "Viaje incentivo que te envia la empresa " + empresa;
+    }
+
+    public String cualquierMetodo2() {
+        return "Método implementado en la clase hija viaje de incentivo";
+
+    }
+
+}
