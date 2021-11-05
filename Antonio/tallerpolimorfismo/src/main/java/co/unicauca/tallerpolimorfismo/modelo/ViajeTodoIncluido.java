f4594750c583580aa8d439c9a7b5b39207dde038
Antonio Estrada <antonioestrada@unicauca.edu.co>
2021-11-05 17:03:51 -0500

    Antonio: Practica 1 polimorfismo

---
+++ Antonio/tallerpolimorfismo/src/main/java/co/unicauca/tallerpolimorfismo/modelo/ViajeTodoIncluido.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.unicauca.tallerpolimorfismo.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author qwert
+ */
+public class ViajeTodoIncluido extends Viaje {
+// Constructores
+
+    
+    public ViajeTodoIncluido(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+    }
+
+    @Override
+    public String descripcion() {
+        return "Disfruta tu viaje todo incluido";
+    }
+//No se sobreescribe cualquierMetodo2()
+}
