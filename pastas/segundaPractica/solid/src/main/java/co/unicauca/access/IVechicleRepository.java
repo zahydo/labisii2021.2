6198109ee99c5e2ba7dbfffb46c0472aceef86d3
pdaniel102 <pdaniel@unicauca.edu.co>
2021-11-17 13:04:57 -0500

    methods declared in access and domain classes, main cleared

--- pastas/segundaPractica/solid/src/main/java/co/unicauca/access/IVechicleRepository.java
+++ pastas/segundaPractica/solid/src/main/java/co/unicauca/access/IVechicleRepository.java
@@ -1,5 +1,12 @@
 package co.unicauca.access;
 
+import java.util.List;
+
+import co.unicauca.domain.Vehicle;
+
 public interface IVechicleRepository {
     
+    public boolean save(Vehicle newVehiculo);
+
+    public List<Vehicle> list();
 }
--- pastas/segundaPractica/solid/src/main/java/co/unicauca/domain/Vehicle.java
+++ pastas/segundaPractica/solid/src/main/java/co/unicauca/domain/Vehicle.java
@@ -1,5 +1,44 @@
 package co.unicauca.domain;
 
 public class Vehicle {
+    String plate;
+    TypeEnum type;
+
+    
+    public Vehicle() {
+    }
+
+
+    public Vehicle(String plate, TypeEnum type) {
+        this.plate = plate;
+        this.type = type;
+    }
+
+
+    public String getPlate() {
+        return plate;
+    }
+
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+
+    public TypeEnum getType() {
+        return type;
+    }
+
+
+    public void setType(TypeEnum type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Vehicle [plate=" + plate + ", type=" + type + "]";
+    }
+
     
 }
