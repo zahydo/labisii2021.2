b5d99c69ad313a20f96a5aeeaecfa2ce4fffa975
UnSup_ <collazosdie@unicauca.edu.co>
2021-11-17 12:53:10 -0500

    pull

--- AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/MotoParkingCost.java
+++ AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/MotoParkingCost.java
@@ -19,28 +19,6 @@ public class MotoParkingCost implements IParkingCost{
             double costo = 2000 + (horas - 1) * 1000;
             cost = (long) Math.ceil(costo / 1000) * 1000;
         }
-<<<<<<< HEAD
-        return cost;
-=======
         return cost; 
-    }
-    
-    public void sorteo(int number){
-        int valor = (int)Math.floor(Math.random()*1001);  
-        if(valor< 0 || valor > 1000){
-            informacionSorteo = "número inválido";
-            ganoSorteo  = false; 
-        }else if(valor == number){
-            informacionSorteo = "Ganó sorteo"; 
-            ganoSorteo = true;  
-        }else {
-            informacionSorteo = "no ganó sorteo"; 
-            ganoSorteo = false; 
-        }
-    }
-        
-    public String getInformacionSorteo(){
-        return informacionSorteo;  
->>>>>>> 49ae8005f19ac53341d69a99d21d49ab9b65ceca
     }
 }
--- AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/TruckParkingCost.java
+++ AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/TruckParkingCost.java
@@ -3,11 +3,6 @@ package co.unicauca.parkinglot.domain;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-<<<<<<< HEAD
-/**
- *
- * @author ASUS
- */
 public class TruckParkingCost implements IParkingCost {
 
     String informacionSorteo = "";
@@ -50,25 +45,4 @@ public class TruckParkingCost implements IParkingCost {
         return informacionSorteo;
     }
 
-=======
-public class TruckParkingCost implements IParkingCost{
-    public TruckParkingCost() {
-    }
-    @Override
-    public long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output) {
-        Duration d1 = Duration.between(input, output); 
-        double horas = d1.toMinutes()/60; 
-        long cost = 0; 
-        
-        if(horas > 12 && horas <= 24){
-            cost = 15000; 
-        }else if(horas <= 12){
-            cost = 10000; 
-        }else {
-            double costodias = (horas/24.00)*15000;  
-            cost = (long)Math.ceil(costodias/1000)*1000;  
-        }
-        return cost; 
-    }
->>>>>>> 49ae8005f19ac53341d69a99d21d49ab9b65ceca
 }
