6cf30634c80743f2a1cfd429da1bd58a7721eb81
UnSup_ <collazosdie@unicauca.edu.co>
2021-11-17 10:22:45 -0500

    AndresMunoz_DiegoCollazos-AB-Principios_Solid: presentacion done

--- AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co.unicauca.parkinglot.presentacion/ClientMain.java
+++ AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co.unicauca.parkinglot.presentacion/ClientMain.java
@@ -1,7 +1,29 @@
 package co.unicauca.parkinglot.presentacion;
 
+import co.unicauca.parkinglot.access.IVehicleRepository;
+import co.unicauca.parkinglot.access.RepositoryFactory;
+import co.unicauca.parkinglot.domain.Vehicle;
+import co.unicauca.parkinglot.domain.TypeEnum;
+import co.unicauca.parkinglot.domain.service.Service;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
 public class ClientMain {
     public static void main(String[] args) {
-        System.out.println(1.424%1);
+        Vehicle veh = new Vehicle("FTK-123", TypeEnum.MOTO);
+        LocalDateTime input = LocalDateTime.of(2021, Month.FEBRUARY, 22, 8, 0);
+        LocalDateTime output = LocalDateTime.of(2021, Month.FEBRUARY, 22, 19, 30);
+        IVehicleRepository repo = RepositoryFactory.getInstance().getRepository("default");
+        Service service = new Service(repo); //Inyección de dependencias
+        long result = service.calculateParkingCost(veh, input, output);
+        System.out.println("Valor a pagar por la moto: " + result);
+        service.saveVehicle(veh);
+        veh = new Vehicle("JNK-124", TypeEnum.CAR);
+        service.saveVehicle(veh);
+        List<Vehicle> list = service.listVehicles();
+        list.forEach(vehicle -> {
+        System.out.println(vehicle.toString());
+        });
     }
 }
