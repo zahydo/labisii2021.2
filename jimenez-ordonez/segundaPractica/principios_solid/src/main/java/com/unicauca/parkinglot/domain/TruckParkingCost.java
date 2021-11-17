385f81751db7341ffa4e5b60afa11ffe61c8c2fa
juleMay <jandresordonez@unicauca.edu.co>
2021-11-17 15:56:21 -0500

    Tercer commit

--- jimenez-ordonez/segundaPractica/principios_solid/src/main/java/com/unicauca/parkinglot/domain/TruckParkingCost.java
+++ jimenez-ordonez/segundaPractica/principios_solid/src/main/java/com/unicauca/parkinglot/domain/TruckParkingCost.java
@@ -1,13 +1,22 @@
 package com.unicauca.parkinglot.domain;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class TruckParkingCost implements IParkingCost {
 
     @Override
     public long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output) {
-        // TODO Auto-generated method stub
-        return 0;
+        Duration varDur = Duration.between(input, output);
+        if (varDur.toHours() < 12){
+            return 10000;
+        } else if (varDur.toHours() > 12 && varDur.toHours() < 24){
+            return 15000;
+        } else {
+            long varDays = varDur.toDays();
+            long varHours = varDur.minus(Duration.ofDays(varDays)).toHours();
+            return (long) Math.ceil( (15000*varDays) + ((varHours*15000)/24));
+        }
     }
     
 }
