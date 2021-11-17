e3dae91551fdd06e7017fb6fcb30a08f3cd20879
dohimenezg <djimenezg@unicauca.edu.co>
2021-11-17 15:36:17 -0500

    Se impelementa servivios

--- jimenez-ordonez/segundaPractica/principios_solid/src/main/java/com/unicauca/parkinglot/infra/Utilities.java
+++ jimenez-ordonez/segundaPractica/principios_solid/src/main/java/com/unicauca/parkinglot/infra/Utilities.java
@@ -2,22 +2,13 @@ package com.unicauca.parkinglot.infra;
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.unicauca.parkinglot.domain.TypeEnum;
 
 public class Utilities {
 
-    public static Map<String, Long> calculateDuration(LocalDate input, LocalDate output) {
-        Duration duration = (input.compareTo(output) == -1) ? (Duration.between(input, output)) : null;
-        if (duration != null) {
-            Map<String, Long> map = new HashMap<String, Long>();
-            map.put("hours", duration.toHours());
-            map.put("minutes", (duration.toMinutes() % 60));
-            return map;
-        }
-        return null;
+    public static Duration calculateDuration(LocalDate input, LocalDate output) {
+        return (input.compareTo(output) == -1) ? (Duration.between(input, output)) : null;
     }
 
     public static String enumToString(TypeEnum type){
