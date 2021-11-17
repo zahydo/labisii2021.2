b4b39459ea69ca116afac40d1701d8d3b0ccc7b1
pdaniel102 <pdaniel@unicauca.edu.co>
2021-11-17 12:39:30 -0500

    Parking Cost interface methods implemented

--- pastas/segundaPractica/solid/src/main/java/co/unicauca/domain/IParkingCost.java
+++ pastas/segundaPractica/solid/src/main/java/co/unicauca/domain/IParkingCost.java
@@ -1,5 +1,7 @@
 package co.unicauca.domain;
 
-public class IParkingCost {
-    
+import java.time.LocalDateTime;
+
+public interface IParkingCost {
+    public abstract long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output);
 }
--- pastas/segundaPractica/solid/src/main/java/co/unicauca/domain/MotoParkingCost.java
+++ pastas/segundaPractica/solid/src/main/java/co/unicauca/domain/MotoParkingCost.java
@@ -1,5 +1,45 @@
 package co.unicauca.domain;
 
-public class MotoParkingCost {
-    
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+public class MotoParkingCost implements IParkingCost {
+
+    static final long MIN_RATE = 1000;
+    static final long FIXED_RATE = 2000;
+
+    public MotoParkingCost() {
+    };
+
+    @Override
+    public long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output) {
+
+        long hours = 0, minutes = 0, cost = 0;
+
+        Duration duration = Duration.between(input, output);
+        double rate;
+
+        if (duration.toHours() < 1) {
+            return MIN_RATE;
+        } else {
+
+            hours = duration.toHours();
+            long extra_hours = 0;
+
+            if (hours >= 2) {
+                extra_hours = MIN_RATE * (hours - 1);
+            }
+
+            minutes = ((duration.getSeconds() % 3600) / 60);
+
+            rate = (double) minutes / 60;
+
+            long extra = (long) (rate * MIN_RATE - (rate * MIN_RATE % 100)) + 100;
+
+            cost = (long) (FIXED_RATE + extra_hours + extra);
+
+            return cost;
+        }
+    }
+
 }
