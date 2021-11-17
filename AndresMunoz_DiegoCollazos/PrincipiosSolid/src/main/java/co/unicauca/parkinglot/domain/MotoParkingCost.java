389b4fcf0c03a2c2fbd8d2400f3794754c3b25b4
Tuldrum <90346628+Tuldrum@users.noreply.github.com>
2021-11-17 12:44:38 -0500

    collazosperafan_muñoz: actualizacion

--- AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/MotoParkingCost.java
+++ AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/MotoParkingCost.java
@@ -4,48 +4,21 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class MotoParkingCost implements IParkingCost{
-
-    String informacionSorteo = "";  
-    boolean ganoSorteo = false; 
-    
     
     @Override
     public long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output) {
-        Duration d1 = Duration.between(input, output); 
-        double horas = d1.toMinutes()/60.00;
-        long cost = 0; 
-        if(ganoSorteo){
-            return 0; 
-        }
-        if(horas < 1){
-            cost = 1000; 
-        }else if(horas == 1){
-            cost = 2000;
-        }else{
-            double costo = 2000 + (horas - 1)*1000;
-            cost = (long)Math.ceil(costo/1000)*1000;
-        }
-        return cost; 
-    }
-<<<<<<< HEAD
-=======
-    
-    public void sorteo(int number){
-        int valor = (int)Math.floor(Math.random()*1001);  
-        if(valor< 0 || valor > 1000){
-            informacionSorteo = "número inválido";
-            ganoSorteo  = false; 
-        }else if(valor == number){
-            informacionSorteo = "Ganó sorteo"; 
-            ganoSorteo = true;  
-        }else {
-            informacionSorteo = "no ganó sorteo"; 
-            ganoSorteo = false; 
-        }
-    }
+        Duration d1 = Duration.between(input, output);
+        double horas = d1.toMinutes() / 60.00;
+        long cost = 0;
         
-    public String getInformacionSorteo(){
-        return informacionSorteo;  
+        if (horas < 1) {
+            cost = 1000;
+        } else if (horas == 1) {
+            cost = 2000;
+        } else {
+            double costo = 2000 + (horas - 1) * 1000;
+            cost = (long) Math.ceil(costo / 1000) * 1000;
+        }
+        return cost;
     }
->>>>>>> 4e34ea1 (collazosperafan_muñoz: actualizacion ../parkinglot/infra/Utilities.java)
 }
--- AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/TruckParkingCost.java
+++ AndresMunoz_DiegoCollazos/PrincipiosSolid/src/main/java/co/unicauca/parkinglot/domain/TruckParkingCost.java
@@ -5,19 +5,53 @@
  */
 package co.unicauca.parkinglot.domain;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-
 
 /**
  *
  * @author ASUS
  */
-public class TruckParkingCost implements IParkingCost{
+public class TruckParkingCost implements IParkingCost {
+
+    String informacionSorteo = "";
+    boolean ganoSorteo = false;
 
     @Override
     public long calculateCost(Vehicle veh, LocalDateTime input, LocalDateTime output) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Duration d1 = Duration.between(input, output);
+        double horas = d1.toMinutes() / 60.00;
+        long cost = 0;
+        if (ganoSorteo) {
+            return 0;
+        }
+        if (horas <= 12) {
+            cost = 10000;
+        } else if (12 < horas && horas <= 24) {
+            cost = 15000; 
+        } else {
+            double costo = (horas/24.00) * 15000; 
+            cost = (long) Math.ceil(costo / 1000) * 1000;
+        }
+        return cost;
     }
 
-    
+    public void sorteo(int number) {
+        int valor = (int) Math.floor(Math.random() * 1001);
+        if (valor < 0 || valor > 1000) {
+            informacionSorteo = "número inválido";
+            ganoSorteo = false;
+        } else if (valor == number) {
+            informacionSorteo = "Ganó sorteo";
+            ganoSorteo = true;
+        } else {
+            informacionSorteo = "no ganó sorteo";
+            ganoSorteo = false;
+        }
+    }
+
+    public String getInformacionSorteo() {
+        return informacionSorteo;
+    }
+
 }
