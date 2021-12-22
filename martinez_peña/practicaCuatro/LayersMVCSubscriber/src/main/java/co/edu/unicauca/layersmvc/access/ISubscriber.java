726f7cc5437255d8acdb5e90eb6f1a1cde7bf975
Paula <ppena@unicauca.edu.co>
2021-12-22 11:31:48 -0500

    martinez_peña : cuarta practica Patrón publicador-suscriptor

---
+++ martinez_peña/practicaCuatro/LayersMVCSubscriber/src/main/java/co/edu/unicauca/layersmvc/access/ISubscriber.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicauca.layersmvc.access;
+
+/**
+ *
+ * @author 57322
+ */
+public interface ISubscriber {
+    
+    public void Subscriber() throws Exception;
+    
+}
