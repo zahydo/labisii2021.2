e59113a7d02e8dbb218eff6600e24b9b61bb0da1
Esteban R <70593544+XzzIsor@users.noreply.github.com>
2022-02-18 01:01:16 -0500

    Cerón_GrupoB_Ruiz_CommandPattern

---
+++ Gomez_FMartinez_Ruiz/Publisher-Suscriber-Pattern/LayersMVC/src/main/java/co/edu/unicauca/layersmvc/infra/IPublisher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unicauca.layersmvc.infra;
+
+import co.edu.unicauca.layersmvc.domain.Product;
+
+/**
+ *
+ * @author XssIsor
+ */
+public interface IPublisher {
+    
+    public void publish(Product product);
+    
+}
