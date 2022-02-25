2b6f4ae69a242cc89e7a18dd2ebabc2613f7717c
Paula <ppena@unicauca.edu.co>
2022-02-25 15:27:52 -0500

    martinez_peña|Adapter-decorator-pattern : sexta practica

---
+++ martinez_peña/sextaPractica/PatronAdapterYDecorator_juliansmartinez_ppena/src/main/java/co/edu/unicauca/commandrestaurant/domain/UpdateCommand.java
@@ -0,0 +1,55 @@
+package co.edu.unicauca.commandrestaurant.domain;
+
+import co.edu.unicauca.commandrestaurant.domain.service.FoodService;
+import co.edu.unicauca.commandrestaurant.access.RepositoryFactory;
+import co.edu.unicauca.commandrestaurant.access.IFoodRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Comando concreto para modificar una comida
+ *
+ * @author Libardo Pantoja, Julio A. Hurtado
+ */
+public class UpdateCommand extends FoodCommand {
+
+    /**
+     * Comida a ser modificada
+     */
+    private Food food;
+    /**
+     * Comida previa, que permitirá deshacer la operación de modificar
+     */
+    private Food foodPrevious;
+    /**
+     * Constructor parametrizado
+     * @param food comida a modificar
+     */
+    public UpdateCommand(Food food) {
+        this.food = food;
+        this.hasUndo = true;
+    }
+
+    @Override
+    public void execute() {
+        Logger logger= LoggerFactory.getLogger(UpdateCommand.class); 
+        logger.info("Comando de edición se ha ejecutado. Se grabó la comida " + food);            
+        service.update(food);
+    }
+
+    @Override
+    public void undo() {
+        Logger logger= LoggerFactory.getLogger(UpdateCommand.class); 
+        logger.info("Comando de edición se ha deshecho. Se grabó la comida " + foodPrevious);             
+        service.update(foodPrevious);
+    }
+
+    public Food getFoodPrevious() {
+        return foodPrevious;
+    }
+
+    public void setFoodPrevious(Food componentPrevious) {
+        this.foodPrevious = componentPrevious;
+    }
+
+}
