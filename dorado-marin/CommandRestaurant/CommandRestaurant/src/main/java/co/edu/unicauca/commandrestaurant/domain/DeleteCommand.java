387c6b79ea480b564122a810156d48fa6a47b905
fernandorado <hfdorado@unicauca.edu.co>
2022-03-04 23:38:34 -0500

    HectorDorado-B-FabianMarin-Command
    Se hace entrega del patrón command.

---
+++ dorado-marin/CommandRestaurant/CommandRestaurant/src/main/java/co/edu/unicauca/commandrestaurant/domain/DeleteCommand.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicauca.commandrestaurant.domain;
+
+import co.edu.unicauca.commandrestaurant.access.IFoodRepository;
+import co.edu.unicauca.commandrestaurant.access.RepositoryFactory;
+import co.edu.unicauca.commandrestaurant.domain.service.FoodService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Fabián David Marín y Héctor Fernando Dorado
+ */
+public class DeleteCommand extends Command{
+
+    /**
+     * Comida a ser eliminada
+     */
+    private Food food;
+    
+    /**
+     * comida previa, que permitirá deshacer la operación de eliminar
+     */ 
+    private Food foodPrevious;
+    
+    /**
+     * Instancia al receptor
+     */
+    private FoodService service;
+  
+    /**
+     * Constructor parametrizado
+     * @param food comida a eliminar
+     */  
+    public DeleteCommand(Food food) {
+        this.food = food;
+        IFoodRepository repo = RepositoryFactory.getInstance().getRepository("default");
+        service = new FoodService(repo);
+        this.hasUndo = true;
+    }
+    
+    @Override
+    public void execute() {
+        Logger logger= LoggerFactory.getLogger(DeleteCommand.class); 
+        logger.info("Comando de eliminación se ha ejecutado. Se eliminó la comida " + food);            
+        service.delete(food.getId());
+    }
+
+    @Override
+    public void undo() {
+        Logger logger= LoggerFactory.getLogger(DeleteCommand.class); 
+        logger.info("Comando de eliminación se ha deshecho. no se eliminó la comida " + foodPrevious);             
+        service.create(foodPrevious);
+    }
+    
+    public Food getFoodPrevious() {
+        return foodPrevious;
+    }
+
+    public void setFoodPrevious(Food componentPrevious) {
+        this.foodPrevious = componentPrevious;
+    }
+    
+}
---
+++ dorado-marin/CommandRestaurant/README.md
@@ -0,0 +1,18 @@
+labisii2021.1
+
+## Nombre completo
+
+Hector Fernando Dorado Sapuyes y Fabian David Marin
+
+## Grupo de trabajo
+
+B
+
+## Práctica
+
+Patrón Command
+
+## Objetivo
+
+Se implementa la funcionalidad del botón eliminar y modficar por medio del patrón command, 
+permitiendo que si una accion se elimina recuperarlo con modificar.
