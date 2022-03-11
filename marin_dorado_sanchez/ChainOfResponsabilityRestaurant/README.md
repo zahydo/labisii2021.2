5db8a7e6f529d06fb4a8090ceb45603339985547
ssbastian <62351126+ssbastian@users.noreply.github.com>
2022-03-11 15:48:45 -0500

    add Patrón Chain of Responsibility

---
+++ marin_dorado_sanchez/ChainOfResponsabilityRestaurant/README.md
@@ -0,0 +1,17 @@
+labisii2021.1
+
+## Nombre completo
+
+	-Hector Fernando Dorado Sapuyes 
+	-Fabian David Marin
+	-Juan Sebastian Sanchez
+## Grupo de trabajo
+B
+
+## Práctica
+Patrón Chain of Responsability (CoR)
+
+## Objetivo
+
+Conocer y aplicar el patrón Chain Of Responsability para el diseño de sistemas software
+flexibles y mantenibles.
---
+++ marin_dorado_sanchez/ChainOfResponsabilityRestaurant/src/main/java/co/edu/unicauca/cor/domain/LevelFour.java
@@ -0,0 +1,40 @@
+/**
+ * clase implementada para que funcione el cuarto nivel
+ * 
+ */
+package co.edu.unicauca.cor.domain;
+
+
+import co.edu.unicauca.cor.infra.Utilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Fabián David Marín - Hector Fernando Dorado - Juan Sebastian Sanchez
+ */
+public class LevelFour extends ClaimHandler{
+    
+    /**
+     * Constructor parametrizado
+     * @param email 
+     */
+    public LevelFour(String email) {
+        super(email);
+    }
+
+    @Override
+    public boolean attend(Claim claim) {
+        if (claim.getType().equals(TypeEnum.HIGHER)) {
+            Logger logger = LoggerFactory.getLogger(Utilities.class);
+            logger.info("El reclamo será atendido en el nivel 4 por " + getEmail());
+            Utilities.sendMail(getEmail(), claim.getTitle(), claim.getDescription());
+            claim.setAttended(true);
+            return true;
+
+        } else {
+            System.out.println("No se puede atender. El reclamo está sin clasificar");
+            return false;
+        }
+    }
+}
