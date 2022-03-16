eaf0a3119708b70c90ff058459d5aaeb8ccfccdf
Anderson Diaz <andersondiaz@unicauca.edu.co>
2022-03-15 19:26:40 -0500

    GARCIA_GA_DIAZ_PatronChainOfResponsibility: Implementacion del patron ChainOfResponsybility

---
+++ Garcia_Diaz/ChainOfResponsabilityRestaurant/README.md
@@ -0,0 +1,17 @@
+# Patrón Change of Responsability
+## Realizado en Java - Maven
+
+Con el fin de ejecutar el programa debera seguir los siguientes pasos:
+
+## Posicionarse en la ubicacion del proyecto: 
+```
+cd ...\labisii2021.2\Garcia_Diaz\ChainOfResponsabilityRestaurant
+```
+## Para construir los jar y ejecutar el proyecto: 
+```
+mvn clean install package
+```
+## Para ejecutar proyecto: 
+```
+mvn exec:java -Dexec.mainClass="co.edu.unicauca.cor.app.Main"
+```
---
+++ Garcia_Diaz/ChainOfResponsabilityRestaurant/src/main/java/co/edu/unicauca/cor/domain/ClaimManager.java
@@ -0,0 +1,60 @@
+package co.edu.unicauca.cor.domain;
+
+/**
+ *
+ * @author Libardo, Julio
+ */
+public class ClaimManager {
+
+    private LevelOne levelOne;
+    private LevelTwo levelTwo;
+    private LevelThree levelThree;
+    private LevelFour levelFour;
+
+   
+
+    public void createAthentionFlow() {
+        levelOne = new LevelOne("mariaortega@gmail.com");
+        levelTwo = new LevelTwo("oscarsanchez@gmail.com");
+        levelThree = new LevelThree("kate@gmail.com");
+        levelFour = new LevelFour("Jaime@gmail.com");
+        // Crea los enlaces
+        levelOne.setNextHandler(levelTwo);
+        levelTwo.setNextHandler(levelThree);
+        levelThree.setNextHandler(levelFour);
+        
+    }
+
+    public LevelOne getLevelOne() {
+        return levelOne;
+    }
+
+    public void setLevelOne(LevelOne levelOne) {
+        this.levelOne = levelOne;
+    }
+
+    public LevelTwo getLevelTwo() {
+        return levelTwo;
+    }
+
+    public void setLevelTwo(LevelTwo levelTwo) {
+        this.levelTwo = levelTwo;
+    }
+
+    public LevelThree getLevelThree() {
+        return levelThree;
+    }
+
+    public void setLevelThree(LevelThree levelThree) {
+        this.levelThree = levelThree;
+    }
+    
+    public LevelFour getLeverFour() {
+        return levelFour;
+    }
+
+    public void setLeverFour(LevelFour leverFour) {
+        this.levelFour = leverFour;
+    }
+
+}
